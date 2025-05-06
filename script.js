fe0057a5ca4506fe8ea4dba1c52b9c54580b10bf
New-user-now <talooli11711@gmail.com>
2025-05-06 16:12:25 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to add interactivity
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button Clicked! Welcome to the interactive webpage!');
+});
